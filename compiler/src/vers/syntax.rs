8a6f84b48d7a6276e82b29533d6f6d7968d9b73f
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-03-23 16:01:48 -0400

    Compiler: worked on syntax.

--- compiler/src/vers/syntax.rs
+++ compiler/src/vers/syntax.rs
@@ -2,12 +2,12 @@ use crate::arch::*;
 use crate::messages::{errors::compiler_error, messages::*};
 use crate::vers::compile::LINE_NUMBER;
 
-pub(crate) fn check_for_semicolon(word: &str) -> bool {
+pub(crate) fn check_for_semicolon(word: &str) -> &str {
     if word.contains(";") {
-        return true;
+        "\n"
     } else {
         unsafe { compiler_error(LINE_NUMBER, E4V, format_args!("{}", "")) }
-        return false;
+        compiler_error(unsafe { LINE_NUMBER }, E4V, format_args!("{}", "Missing semicolon\n"))
     }
 }
 
